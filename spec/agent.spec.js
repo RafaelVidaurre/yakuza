c3b18f4382d09910f15fe50c902d9eb3aea253b9
Rafael Vidaurre <narzerus@gmail.com>
2014-10-21 22:22:35 -0300

    Prepare agent test

---
+++ spec/agent.spec.js
@@ -0,0 +1,13 @@
+var Agent = require('../scraper');
+var _ = require('underscore');
+
+describe('Agent', function () {
+  var agent;
+  beforeEach(function () {
+    agent = new Agent();
+  });
+
+  describe('#Agent', function () {
+
+  });
+});
