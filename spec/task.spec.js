c109b54c3246bbddfe7940fa5437c389e5fe48d1
Rafael Vidaurre <narzerus@gmail.com>
2014-11-20 17:25:01 -0300

    Add new task tests

--- spec/task.spec.js
+++ spec/task.spec.js
@@ -47,4 +47,54 @@ describe('Task', function () {
       expect(task._main).toHaveBeenCalledWith(jasmine.any(Object), task._params);
     });
   });
+
+  describe('#_onShare', function () {
+    it('should set value to the key passed in _sharedStorage', function () {
+      task._onShare('foo', 1);
+      task._onShare('bar', 'value');
+      task._onShare('hello', {a: 1, b: 'test'});
+      task._onShare('overwritten', 1);
+      task._onShare('overwritten', 2);
+      expect(task._sharedStorage.foo).toEqual(1);
+      expect(task._sharedStorage.bar).toEqual('value');
+      expect(task._sharedStorage.hello).toEqual({a: 1, b: 'test'});
+      expect(task._sharedStorage.overwritten).toEqual(2);
+    });
+  });
+
+  describe('#_onSuccess', function () {
+    it('should resolve its running promise', function () {
+      var response = {test: 'stuff'};
+      spyOn(task._runningDeferred, 'resolve');
+      spyOn(task._runningDeferred, 'reject');
+      task._onSuccess(response);
+      expect(task._runningDeferred.resolve).toHaveBeenCalledWith(response, jasmine.any(Object));
+      expect(task._runningDeferred.reject).not.toHaveBeenCalled();
+    });
+
+    it('should pass its shared storage in the resolve response', function () {
+      spyOn(task._runningDeferred, 'resolve');
+      task._sharedStorage = {a: 1};
+      task._onSuccess(1);
+      expect(task._runningDeferred.resolve).toHaveBeenCalledWith(1, task._sharedStorage);
+    });
+  });
+
+  describe('#_onError', function () {
+    it('it should reject its running promise', function () {
+      spyOn(task._runningDeferred, 'resolve');
+      spyOn(task._runningDeferred, 'reject');
+      task._onError();
+      expect(task._runningDeferred.resolve).not.toHaveBeenCalled();
+      expect(task._runningDeferred.reject).toHaveBeenCalled();
+    });
+
+    it('it should pass the error and message in the reject response', function () {
+      var error = new Error('test error');
+      var message = 'test message';
+      spyOn(task._runningDeferred, 'reject');
+      task._onError(error, message);
+      expect(task._runningDeferred.reject).toHaveBeenCalledWith(error, message);
+    });
+  });
 });
