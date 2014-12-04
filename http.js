db405d4778260e9f91b4d3d770a24b140b9372e7
Rafael Vidaurre <narzerus@gmail.com>
2014-12-04 17:58:15 -0300

    Prepare tests for http class

--- http.js
+++ http.js
@@ -8,8 +8,37 @@
 
 var request = require('request');
 
-function Http () {
-  // TODO: Implement this class
+function Http (defaultCookies) {
+  /**
+  * Cookie jar for the Http instance
+  * @private
+  */
+  this._cookieJar = defaultCookies || request.jar();
+
+  /**
+  * Instance of mikeal's request API with a default cookie jar set
+  * @private
+  */
+  this._request = request.defaults({jar: this._cookieJar});
+
+  /**
+  * Array of requests logged
+  * @private
+  */
+  this._log = [];
 }
+
+/**
+* Pushes a response object to the request log and makes proper
+* @param {object} err Error object, is null if no error is present
+* @param {object} res Request's response
+* @param {string} body Request's response body only, is null if an error is present
+* @param {function} callback Callback to be executed
+* @private
+*/
+Http.prototype._interceptResponse = function (err, res, body, callback) {
+  this.pushToLog({response: res, body: body});
+  callback(err, res, body);
+};
 
 module.exports = Http;
---
+++ spec/http.spec.js
@@ -0,0 +1,123 @@
+'use strict';
+
+var Http = require('../http');
+var request = require('request');
+
+describe('Http', function () {
+  var http;
+
+  beforeEach(function () {
+    http = new Http();
+  });
+
+  describe('#Http', function () {
+    it('should set default cookies if provided', function () {
+      var jar = request.jar();
+      var newHttp = new Http(jar);
+      expect(newHttp._cookieJar).toBe(jar);
+    });
+
+    it('should set a new cookie jar if no cookes are provided', function () {
+      expect(http._cookieJar).toEqual(request.jar());
+    });
+
+    it('should start with an empty log', function () {
+      expect(http._log).toEqual([]);
+    });
+  });
+
+  describe('#_interceptResponse', function () {
+    it('should call pushToLog', function () {
+      var fakeResponse = {fake: 'response'};
+      var fakeBody = {fake: 'body'};
+      spyOn(http, '_pushToLog');
+      http._interceptResponse(null, fakeResponse, fakeBody);
+      expect(http._pushToLog).toHaveBeenCalledWith({response: fakeResponse, body: fakeBody});
+    });
+
+    it('should call the callback with response data', function () {
+      var test = {callback: function () {}};
+      spyOn(test, 'callback');
+      http._interceptResponse(1, 2, 3, test.callback);
+      expect(test.callback).toHaveBeenCalledWith(1, 2, 3);
+    });
+  });
+
+  describe('#_pushToLog', function () {
+    it('it should push response data to log', function () {
+      var data = {response: 1, body: 2};
+      http._pushToLog(data);
+      expect(http._log).toEqual([data]);
+    });
+  });
+
+  describe('#_initRequestParams', function () {
+    it('should return an object', function () {
+      expect(typeof http._initRequestParams('asd', {asd: 'asd'}) === 'object').toBe(true);
+    });
+
+    it('should return request parameters properly', function () {
+      var cb = function () {};
+      var uri = 'http://www.test.com';
+      var opts = {uri: uri};
+      var res = http._initRequestParams(uri, cb);
+      var expectedRes = {uri: uri, options: {uri: uri}, callback: cb};
+      expect(res).toEqual(expectedRes);
+      res = http._initRequestParams(opts, cb);
+      expect(res).toEqual(expectedRes);
+      res = http._initRequestParams(uri, opts, cb);
+      expect(res).toEqual(expectedRes);
+    });
+  });
+
+  describe('Request delegators', function () {
+    var uri, opts, cb;
+    beforeEach(function () {
+      uri = 'fakeUri';
+      opts = {a: 1};
+      cb = function () {};
+    });
+
+    describe('#del', function () {
+      spyOn(http._request, 'del');
+      http.del('a', opts, cb);
+      expect(http._request.del).toHaveBeenCalledWith(uri, opts, cb);
+    });
+
+    describe('#get', function () {
+      spyOn(http._request, 'get');
+      http.del('a', opts, cb);
+      expect(http._request.get).toHaveBeenCalledWith(uri, opts, cb);
+    });
+
+    describe('#head', function () {
+      spyOn(http._request, 'head');
+      http.del('a', opts, cb);
+      expect(http._request.head).toHaveBeenCalledWith(uri, opts, cb);
+    });
+
+    describe('#patch', function () {
+      spyOn(http._request, 'patch');
+      http.del('a', opts, cb);
+      expect(http._request.patch).toHaveBeenCalledWith(uri, opts, cb);
+    });
+
+    describe('#post', function () {
+      spyOn(http._request, 'post');
+      http.del('a', opts, cb);
+      expect(http._request.post).toHaveBeenCalledWith(uri, opts, cb);
+    });
+
+    describe('#put', function () {
+      spyOn(http._request, 'put');
+      http.del('a', opts, cb);
+      expect(http._request.put).toHaveBeenCalledWith(uri, opts, cb);
+    });
+  });
+
+  describe('#getCookieJar', function () {
+    var newCookieJar = request.jar();
+    var newHttp = new Http(newCookieJar);
+    expect(newHttp.getCookieJar()).toEqual(newCookieJar);
+  });
+});
