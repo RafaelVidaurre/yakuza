b3d4bb364639ee0e7075e954daa5f4c5cba87289
Rafael Vidaurre <narzerus@gmail.com>
2014-12-12 17:35:30 -0300

    Test partially Yakuza module

--- spec/yakuza.spec.js
+++ spec/yakuza.spec.js
@@ -2,6 +2,7 @@
 
 var YakuzaBase = require('../yakuza-base');
 var Scraper = require('../scraper');
+var Job = require('../job');
 var _ = require('lodash');
 
 describe('Yakuza', function () {
@@ -27,6 +28,25 @@ describe('Yakuza', function () {
       expect(Yakuza._scrapers.test).toBe(undefined);
       var scr = Yakuza.scraper('test');
       expect(Yakuza._scrapers.test).toBe(scr);
+    });
+
+    it('should return an existing scraper if the key passed matches a previously created one',
+      function () {
+      var scr = Yakuza.scraper('foo');
+      var scr2 = Yakuza.scraper('foo');
+      var scr3 = Yakuza.scraper('bar');
+
+      expect(scr).toBe(scr2);
+      expect(scr).not.toBe(scr3);
+    });
+  });
+
+  describe('#job', function () {
+    it('should return a job instance', function () {
+      Yakuza.scraper('foo');
+      Yakuza.scraper('foo').agent('bar');
+      var job = Yakuza.job('foo', 'bar');
+      expect(job instanceof Job).toBe(true);
     });
   });
 });
